6395b3fe047cbd2626b24cea87114cfaf11872b2
SereenElghazali <34245211+SereenElghazali@users.noreply.github.com>
2017-12-06 16:43:19 +0200

    new files

---
+++ js/custom1.js
@@ -0,0 +1,77 @@
+
+/*global $, alert, console*/
+
+$(function () {
+    'use strict';
+    // Trigger Nice Scroll Plugin
+    
+    $('.header .arrow i').click(function () {
+       
+        $('html, body').animate({
+            
+            scrollTop: $('.features').offset().top
+            
+        }, 1000);
+        
+    });
+    $('.show-more').click(function () {
+        $('.our-work .hidden').fadeIn(1000);
+        
+    });
+     // Check Testimonials
+    
+    var leftArrow = $('.testim .fa-chevron-left'),
+        
+        rightArrow = $('.testim .fa-chevron-right');
+    
+    function checkClients() {
+        
+        if ($('.client:first').hasClass('active')) {
+            
+            leftArrow.fadeOut();
+            
+        } else {
+            
+            leftArrow.fadeIn();
+        }
+        
+        if ($('.client:last').hasClass('active')) {
+            
+            rightArrow.fadeOut();
+            
+        } else {
+            
+            rightArrow.fadeIn();
+        }
+                
+    }
+    
+    checkClients();
+    
+    $('.testim i').click(function () {
+        
+        if ($(this).hasClass('fa-chevron-right')) {
+            
+            $('.testim .active').fadeOut(100, function () {
+               
+                $(this).removeClass('active').next('.client').addClass('active').fadeIn();
+                
+                checkClients();
+                
+            });
+            
+        } else {
+            
+            $('.testim .active').fadeOut(100, function () {
+               
+                $(this).removeClass('active').prev('.client').addClass('active').fadeIn();
+                
+                checkClients();
+                
+            });
+            
+        }
+        
+    });
+
+});
